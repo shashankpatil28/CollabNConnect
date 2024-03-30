cb640feb5ee6bb79eafd136c35a34e791a02a59f
shashankpatil28 <patilshashank2003@gmail.com>
2024-03-31 02:00:45 +0530

    channels created

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -87,6 +87,7 @@ export const ServerHeader = ({
                     (
                         <DropdownMenuItem
                             className="px-3 py-2 cursor-pointer"
+                            onClick={() => onOpen("createChannel")}
                         >
                             Create Channel
                             <PlusCircle 
