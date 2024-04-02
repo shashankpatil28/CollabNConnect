91bcf284f78008d9c323bc22d7baf9c2d0ba0eba
shashankpatil28 <patilshashank2003@gmail.com>
2024-04-02 19:06:33 +0530

    emoji added

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useModal } from "@/hooks/use-modal-store";
-// import { EmojiPicker } from "@/components/emoji-picker";
+import EmojiPicker from "../emoji-picker";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -75,7 +75,7 @@ export const ChatInput = ({
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    // onClick={() => onOpen("messageFile", { apiUrl, query })}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
@@ -86,11 +86,11 @@ export const ChatInput = ({
                     placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
                     {...field}
                   />
-                  {/* <div className="absolute top-7 right-8">
+                  <div className="absolute top-7 right-8">
                     <EmojiPicker
                       onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
                     />
-                  </div> */}
+                  </div>
                 </div>
               </FormControl>
             </FormItem>
